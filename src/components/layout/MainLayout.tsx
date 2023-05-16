3430865421539a30ee7c9b80c1402e9f3df20b42
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-05-16 15:43:34 -0300

    feat: adicionada responsividade

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
 import React from 'react'
+
+import LRA_logo from '../../../public/LRA_logo.png'
 
 type Props = {
     children: React.ReactNode
@@ -6,14 +9,16 @@ type Props = {
 
 export default function MainLayout({ children }: Props) {
     return (
-        <div className="flex w-full flex-col items-center px-20">
+        <div className="flex w-full flex-col items-center px-4 py-4 sm:px-10 lg:px-20">
             <div className="flex w-full flex-row items-center ">
                 <div className="w-full border-b border-b-blue-900" />
-                <p className="mx-10 text-3xl leading-10">Foto</p>
+                <div className="mx-10">
+                    <Image src={LRA_logo} alt="Logic Resolver Ai" width={195} height={195} />
+                </div>
                 <div className="w-full border-b border-b-blue-900" />
             </div>
 
-            <div className="h-full antialiased w-full">{children}</div>
+            <div className="h-full w-full antialiased">{children}</div>
         </div>
     )
 }
