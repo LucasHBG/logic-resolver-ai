c2577b10b4df4fd51d7a27f9d1a98c7efb068527
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-05-16 15:43:07 -0300

    feat: adicionado botões e estilização

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,7 +3,7 @@ import { Fragment, useState } from 'react'
 import MainLayout from './layout/MainLayout'
 
 export default function HomeComponent(): JSX.Element {
-    const inputText = useState<string>('')
+    const [inputText, setInputText] = useState<string>('')
 
     return (
         <MainLayout>
@@ -17,31 +17,53 @@ export default function HomeComponent(): JSX.Element {
                         id="output"
                         rows={25}
                         autoFocus={false}
-                        placeholder='Esperando pergunta...'
+                        placeholder="Esperando pergunta..."
                         disabled
-                    >
-                        
-                    </textarea>
+                    ></textarea>
                 </div>
 
                 <div>
                     <label className="block text-sm font-medium text-gray-900 dark:text-gray-700" htmlFor="input">
                         Pergunta
                     </label>
-                    <form >
-                        <input
-                            id="input"
-                            autoFocus={true}
-                            className="block w-full snap-y snap-center rounded border border-blue-900 bg-gray-50 p-2 text-gray-900 focus:border-blue-900"
-                        />
+                    <form>
+                        <div className="flex w-full">
+                            <input
+                                id="inputText"
+                                autoFocus={true}
+                                className="block w-full snap-y snap-center rounded-l border border-blue-900 bg-gray-50 p-2 text-gray-900 focus:border-blue-900"
+                            />
+                            <button
+                                type="submit"
+                                className="inline-flex items-center rounded-r bg-blue-700 px-4 py-2.5 text-center text-xs font-medium text-white hover:bg-blue-800 focus:ring-1 focus:ring-blue-200 dark:focus:ring-blue-900"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="h-6 w-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
                     </form>
                 </div>
 
-                <div className="flex w-full">
-                    <button className="rounded border border-blue-900 bg-gray-50 p-2 text-gray-900">Botao</button>
-                    <button type="button" className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path></svg>
-               </button>
+                <div className="flex w-full [&>button]:w-20">
+                    <button
+                        type="button"
+                        onClick={() => setInputText(inputText + '&')}
+                        className="block items-center rounded border border-blue-900 p-2 text-center text-gray-900 hover:bg-gray-50 focus:ring-1 focus:ring-blue-200 dark:focus:ring-blue-900"
+                    >
+                        &
+                    </button>
                 </div>
             </div>
         </MainLayout>
