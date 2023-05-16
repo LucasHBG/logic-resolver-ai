8e3c5249e37772cba7d77d442e6e43dda8c0c914
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-05-16 09:36:22 -0300

    feat: adicionado tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'media',
+    theme: {
+        extend: {},
+      },
+      plugins: [
+        require('@tailwindcss/forms'),
+      ],
+}
