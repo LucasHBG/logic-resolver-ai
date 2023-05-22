3f4f1501587dbdf8806b6acb107954be62532ef9
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-05-22 15:02:21 -0300

    feat: adicionado animação na logo

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -11,8 +11,8 @@ export default function MainLayout({ children }: Props) {
     return (
         <div className="flex w-full flex-col items-center px-4 py-4 sm:px-10 lg:px-20">
             <div className="flex w-full flex-row items-center ">
-                <div className="w-full border-b border-b-blue-900" />
-                <div className="mx-10">
+                <div className="w-full border-b border-b-blue-900 " />
+                <div className="mx-10 animate-pulse transition ease-in-out">
                     <Image src={LRA_logo} alt="Logic Resolver Ai" width={195} height={195} />
                 </div>
                 <div className="w-full border-b border-b-blue-900" />
