94067985594cac2123511b3c9853fd3a42bea938
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-10-24 17:51:42 -0300

    feat: responsividade logo

--- components/header.tsx
+++ components/header.tsx
@@ -12,8 +12,8 @@ export default function Header() {
         <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
             <div className="flex items-center ">
                 <Link href="/" target="_blank" rel="nofollow">
-                    <IconLogicResolverAi className="mr-2 h-8 w-8 dark:hidden" inverted />
-                    <IconLogicResolverAi className="mr-2 hidden h-8 w-8 dark:block" />
+                    <IconLogicResolverAi className="mr-2 h-6 w-6 dark:hidden sm:h-8 sm:w-8" inverted />
+                    <IconLogicResolverAi className="mr-2 hidden h-6 w-6 dark:block sm:h-8 sm:w-8" />
                 </Link>
                 <div className="flex items-center ">
                     <IconSeparator className="h-6 w-6 text-muted-foreground/50" />
