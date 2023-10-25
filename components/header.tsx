80900cd69123ceffe381a275c106d8843627e039
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-10-25 12:12:13 -0300

    chore: mudança no nome da função

--- components/header.tsx
+++ components/header.tsx
@@ -5,15 +5,15 @@ import { cn } from '@/lib/utils'
 
 import { ThemeToggle } from './theme-toggle'
 import { buttonVariants } from './ui/button'
-import { IconGitHub, IconLogicResolverAi, IconSeparator } from './ui/icons'
+import { IconGitHub, IconLogicResolverAiWithBackground, IconSeparator } from './ui/icons'
 
 export default function Header() {
     return (
         <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
             <div className="flex items-center ">
                 <Link href="/" target="_blank" rel="nofollow">
-                    <IconLogicResolverAi className="mr-2 h-6 w-6 dark:hidden sm:h-8 sm:w-8" inverted />
-                    <IconLogicResolverAi className="mr-2 hidden h-6 w-6 dark:block sm:h-8 sm:w-8" />
+                    <IconLogicResolverAiWithBackground className="mr-2 h-6 w-6 dark:hidden sm:h-8 sm:w-8" inverted />
+                    <IconLogicResolverAiWithBackground className="mr-2 hidden h-6 w-6 dark:block sm:h-8 sm:w-8" />
                 </Link>
                 <div className="flex items-center ">
                     <IconSeparator className="h-6 w-6 text-muted-foreground/50" />
