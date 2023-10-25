04372c97bbaa742b9806fbc28b4e3a80244152fb
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-10-25 12:11:35 -0300

    feat: tela vazia após abrir site

---
+++ components/empty-screen.tsx
@@ -0,0 +1,43 @@
+import { UseChatHelpers } from 'ai/react'
+
+import { ExternalLink } from '@/components/external-link'
+import { Button } from '@/components/ui/button'
+import { IconArrowRight } from '@/components/ui/icons'
+
+const exampleMessages = [
+    {
+        heading: 'Faça perguntas diretas',
+        message: `Me mostre a tabela verdade de P -> Q.`,
+    },
+    {
+        heading: 'Peça por explicações sobre a matéria',
+        message: `Explique como Q <-> R pode ser usado em uma frase.`,
+    },
+    {
+        heading: 'Converse sobre exemplos que deseja ver',
+        message: `Crie uma tabela verdade somente com disjunções e condicionais usando as seguintes proposições simples: P, Q e S`,
+    },
+]
+
+export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
+    return (
+        <div className="mx-auto max-w-2xl px-4">
+            <div className="rounded-lg border bg-background p-8">
+                <h1 className="mb-2 text-lg font-semibold">Bem-vindo(a) ao Logic Resolver AI Chatbot!</h1>
+                <p className="mb-2 leading-normal text-muted-foreground">
+                    Projeto open source que usou de referência o modelo de chatbot da <ExternalLink href="https://vercel.com">Vercel</ExternalLink> e
+                    o framework <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>.
+                </p>
+                <p className="leading-normal text-muted-foreground">Você pode começar uma conversa por aqui ou usando os seguintes exemplos:</p>
+                <div className="mt-4 flex flex-col items-start space-y-2">
+                    {exampleMessages.map((message, index) => (
+                        <Button key={index} variant="link" className="h-auto p-0 text-base" onClick={() => setInput(message.message)}>
+                            <IconArrowRight className="mr-2 text-muted-foreground" />
+                            {message.heading}
+                        </Button>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
